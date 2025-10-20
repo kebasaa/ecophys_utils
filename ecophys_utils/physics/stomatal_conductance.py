9813eb6514fa7858ee578fa8873879e5a387c7b7
kebasaa <muellerjonathan@gmx.net>
2025-10-20 17:20:14 +0200

    Bugfixes

--- ecophys_utils/physics/stomatal_conductance.py
+++ ecophys_utils/physics/stomatal_conductance.py
@@ -1,11 +1,13 @@
 def total_water_conductance(T_leaf, P_Pa, E_mmol_m2_s, water_conc_air_mmol_mol):
+    import numpy as np
+    from .physics import calculate_es
     # Unit conversion
     E_mol_m2_s = E_mmol_m2_s * 10**(-3)
     water_conc_air = water_conc_air_mmol_mol * 10**(-3)
     
     # Water concentration inside the leaf [mol H2O mol-1 air]
     # assuming saturation in the sub-stomatal cavity (Monteith 1995)
-    water_conc_sat = calculate_es(T_leaf) / P_Pa
+    water_conc_sat = calculate_es(T_leaf, P_Pa) / P_Pa
     
     # water_conc_air is the water concentration in the ambient air plus the H2O molecules added by transpiration,
     # resulting in leaf-to-air VPD (Preisler et al 2023)
@@ -17,13 +19,15 @@ def total_water_conductance(T_leaf, P_Pa, E_mmol_m2_s, water_conc_air_mmol_mol):
 
 # Buckley (2005)  https://doi.org/10.1111/j.1469-8137.2005.01543.x
 def leaf_conductance(T_leaf, P_Pa, E_mmol_m2_s, water_conc_air_mmol_mol):
+    import numpy as np
+    from .physics import calculate_es
     # Unit conversion
     E_mol_m2_s = E_mmol_m2_s * 10**(-3)
     water_conc_air = water_conc_air_mmol_mol * 10**(-3)
     
     # Water concentration inside the leaf [mol H2O mol-1 air]
     # assuming saturation in the sub-stomatal cavity (Monteith 1995)
-    water_conc_sat = calculate_es(T_leaf) / P_Pa
+    water_conc_sat = calculate_es(T_leaf, P_Pa) / P_Pa
     
     g_w = E_mol_m2_s / (water_conc_sat - water_conc_air)
     #g_tw = E / VPD * 100
